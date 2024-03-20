252d45a017a79fcbb041d33cd87a58ed331ad932
David Basantes <85196670+DAVIDS2405@users.noreply.github.com>
2024-03-20 15:54:00 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
   adapter: vercel()
 });
